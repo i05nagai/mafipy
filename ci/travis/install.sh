07f3d028c5bde970009907276f9b6fbac169a11d
i05nagai <nagai.m.ac@t.titech.ac.jp>
2016-11-30 23:43:55 +0900

    Fix travis CI scripts

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -13,10 +13,11 @@ fi
 if [ "$SKIP_TESTS" == "true" ]; then
     echo "No need to build mafipy when not running the tests"
 else
+	pip install numpy
+	pip install scipy
     # Build mafipy in the install.sh script to collapse the verbose
     # build output in the travis output when it succeeds.
     python --version
     python -c "import numpy; print('numpy %s' % numpy.__version__)"
     python -c "import scipy; print('scipy %s' % scipy.__version__)"
-    python setup.py develop
 fi
