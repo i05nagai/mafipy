21b622663bc4202237c1f9a17d687f8dae4de129
i05nagai <nagai.m.ac@t.titech.ac.jp>
2016-12-03 21:59:18 +0900

    Send coverage report to codeclimate

--- ci/travis/after_success.sh
+++ ci/travis/after_success.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-if [ "$CODECLIMATE_COVERAGE_REPORT" == "true" ]; then
-	codeclimate-test-reporter --token $CODECLIMATE_COVERAGE_TOKEN
+if [ "${CODECLIMATE_COVERAGE_REPORT}" == "true" ]; then
+	codeclimate-test-reporter --token ${CODECLIMATE_COVERAGE_TOKEN}
 fi
