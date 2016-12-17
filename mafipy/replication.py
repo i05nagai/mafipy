9621d72b759dd6b08e292e9dd3e42e357707e252
i05nagai <nagai.m.ac@t.titech.ac.jp>
2016-12-17 16:03:55 +0900

    Delete unused SimpleReplication class

--- mafipy/replication.py
+++ mafipy/replication.py
@@ -4,7 +4,6 @@
 from __future__ import division
 
 import scipy.integrate
-import numpy as np
 import functools
 from . import util
 
@@ -142,75 +141,129 @@ class Replication(object):
     def eval(self):
         """eval
         evaluates value of derivative by replication methods.
+
+        .. math::
+            \mathrm{E}
+            \left[
+                f(S(T))
+            \\right]
+            = f(S(0))
+                + \int_{-\infty}^{S(0)} f^{\prime\prime}(K)p(K)\ d K
+                + \int_{S(0)}^{\infty} f^{\prime\prime}(K)c(K)\ d K
+
+        where
+        :math:`S(t)` is value of underlying at time :math:`t`,
+        :math:`f(\cdot)` is payoff function
+            as a function of underlying,
+        :math:`p(K; S(0), T, \sigma)` is value of put pricer
+            with strike :math:`K`.
+        :math:`c(K; S(0), T, \sigma)` is value of call pricer
+            with strike :math:`K`.
         """
         raise NotImplementedError
 
 
-class SimpleReplication(Replication):
-
-    def __init__(self,
-                 call_pricer,
-                 put_pricer,
-                 payoff_func,
-                 payoff_fhess):
-        self.call_pricer = call_pricer
-        self.put_pricer = put_pricer
-        self.payoff_func = payoff_func
-        self.payoff_fhess = self.payoff_fhess
-
-    def _first_integrand(self, strike):
-        return self.put_pricer(strike) * self.weight(strike)
-
-    def _second_integrand(self, strike):
-        return self.call_pricer(strike) * self.weight(strike)
-
-    def weight(self, strike):
-        return self.payoff_fhess(strike)
-
-    def eval(self, init_swap_rate):
-        integrate = scipy.integrate
-
-        first_term = self.payoff_func(init_swap_rate)
-        second_term = integrate.quad(
-            self._first_integrand, -np.inf, init_swap_rate)
-        third_term = integrate.quad(
-            self._second_integrand, init_swap_rate, np.inf)
-        return first_term + second_term + third_term
-
-
 class AnalyticReplication(Replication):
+    """AnalyticReplication
+    This class calculates replication methods using knowledge of derivative.
+    In mathmetical finance, payoff function is discontinuous function
+    or the function which is diffentialble but cannot differentiate twice.
+    Replication method calculates integration with stochastic weight
+    which may contains derivative of payoff funciton.
+    If integrands have dirac delta function or discontinuous function,
+    numerical integration does not work properly.
+
+    This class calculates numerical integration with
+    pre-calculation of stochastic weight.
+
+    :param callable call_pricer:
+    :param callable put_pricer:
+    :param list analytic_funcs:
+    :param list call_integrands:
+    :param list put_integrands:
+    """
 
     def __init__(self,
                  call_pricer,
                  put_pricer,
                  analytic_funcs=[],
-                 put_integrands=[],
-                 call_integrands=[]):
+                 call_integrands=[],
+                 put_integrands=[]):
+        """__init__
+        """
         self.call_pricer = call_pricer
         self.put_pricer = put_pricer
         self.analytic_funcs = analytic_funcs
-        self.put_integrands = put_integrands
         self.call_integrands = call_integrands
+        self.put_integrands = put_integrands
 
     def _calc_analytic_term(self, init_swap_rate):
+        """_calc_analytic_term
+
+        :param init_swap_rate:
+        """
         return sum([f(init_swap_rate) for f in self.analytic_funcs])
 
     def _calc_integral(self, func, pricer, lower, upper):
+        """_calc_integral
+        calculates following equation:
+
+        .. math::
+            \int_{L{\mathrm{lower}}}^{L_{\mathrm{upper}}} f(x) g(x)\ dx,
+
+        where
+        :math:`f(\cdot)` is `func`,
+        :math:`g(\cdot)` is `pricer`,
+        :math:`L_{\mathrm{lower}}` is `lower`,
+        :math:`L_{\mathrm{upper}}` is `upper`.
+
+        :param callable func:
+        :param callable pricer: call option pricer or put option pricer.
+        :param float lower: lower bound of integral.
+        :param float upper: upper bound of integral.
+        :return: integral value.
+        :rtype: float.
+        """
         integrate = scipy.integrate
         result = integrate.quad(lambda x: func(x) * pricer(x), lower, upper)
         return result[0]
 
     def _calc_put_integral(self, init_swap_rate, min_put_range):
+        """_calc_put_integral
+        integrate function with put pricer.
+
+        :param init_swap_rate:
+        :param min_put_range:
+        :return: integal value.
+        :rtype: float.
+        """
         return sum([self._calc_integral(f, self.put_pricer,
                                         min_put_range, init_swap_rate)
                     for f in self.put_integrands])
 
     def _calc_call_integral(self, init_swap_rate, max_call_range):
+        """_calc_call_integral
+        integrate function with put pricer.
+
+        :param init_swap_rate:
+        :param max_call_range:
+        :return: integal value.
+        :rtype: float.
+        """
         return sum([self._calc_integral(f, self.call_pricer,
                                         init_swap_rate, max_call_range)
                     for f in self.call_integrands])
 
     def eval(self, init_swap_rate, min_put_range, max_call_range):
+        """eval
+        evaluates expectation by the replication method.
+
+        :param init_swap_rate:
+        :param min_put_range:
+        :param max_call_range:
+        :return: value of expectation.
+        :rtype: float.
+        """
         analytic_term = self._calc_analytic_term(init_swap_rate)
         put_integral_term = self._calc_put_integral(
             init_swap_rate, min_put_range)
--- mafipy/tests/test_replication.py
+++ mafipy/tests/test_replication.py
@@ -105,3 +105,71 @@ class TestLinearAnnuityMappingFuncHelper(object):
             swap_rate, self.alpha0, self.alpha1)
         actual = self.target.make_fhess()(swap_rate)
         assert expect == approx(actual)
+
+
+class TestAnalyticReplication(object):
+
+    # before all tests starts
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    # after all tests finish
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    # before each test start
+    def setup(self):
+        self.call_pricer = self._func1
+        self.put_pricer = self._func1
+        self.analytic_funcs = [self._func1]
+        self.call_integrands = [self._func1]
+        self.put_integrands = [self._func1]
+        self.target = target.AnalyticReplication(self.call_pricer,
+                                                 self.put_pricer,
+                                                 self.analytic_funcs,
+                                                 self.call_integrands,
+                                                 self.put_integrands)
+
+    # after each test finish
+    def teardown(self):
+        pass
+
+    def _func1(self, x):
+        return x
+
+    def _integrate_func1(self, x):
+        return 0.5 * x * x
+
+    def test_eval(self):
+
+        # integral range is 0
+        def case1():
+            init_swap_rate = util.get_real()[0]
+            min_put_range = init_swap_rate
+            max_call_range = init_swap_rate
+            actual = self.target.eval(
+                init_swap_rate, min_put_range, max_call_range)
+            expect = init_swap_rate
+            assert expect == approx(actual)
+        case1()
+
+        # normal
+        def case2():
+            data = sorted(util.get_real(3))
+            init_swap_rate = data[1]
+            min_put_range = data[0]
+            max_call_range = data[2]
+            actual = self.target.eval(
+                init_swap_rate, min_put_range, max_call_range)
+            # expect
+            expect_analytic = init_swap_rate
+            # put_pricer * term
+            expect_put = (((init_swap_rate ** 3.0) - (min_put_range ** 3.0))
+                          / 3.0)
+            expect_call = (((max_call_range ** 3.0) - (init_swap_rate ** 3.0))
+                           / 3.0)
+            expect = expect_analytic + expect_put + expect_call
+            assert expect == approx(actual)
+        case2()
