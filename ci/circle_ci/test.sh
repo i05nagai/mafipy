b2cb156f34d699eb6d5bf46b2eabb99281feb54e
i05nagai <i05nagai@gmail.com>
2017-01-15 14:15:13 +0900

    Change asv settings

--- ci/circle_ci/test.sh
+++ ci/circle_ci/test.sh
@@ -6,7 +6,7 @@ set -x
 if [ "${BENCHMARK_TEST}" = "true" ]; then
 	# run benchmarks test
 	cd benchmarks
-	python run.py run --skip-existing-commits ALL
+	python run.py run NEW
 	python run.py publish
 	cd ..
 fi
