eeb36d47c84d4b50fefa4d4831c6b9a8a531bb38
i05nagai <i05nagai@gmail.com>
2017-01-17 08:04:30 +0900

    Fix circle ci scripts

--- ci/circle_ci/test.sh
+++ ci/circle_ci/test.sh
@@ -4,9 +4,10 @@ set -e
 set -x
 
 if [ "${BENCHMARK_TEST}" = "true" ]; then
-	# for detached HEAD
 	pushd benchmarks/asv_files
+	# for detached HEAD
 	git checkout master
+	git pull origin master
 	popd
 
 	# run benchmarks test
