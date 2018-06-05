c29ba015e1151f68379b5bdc7fbb7695e2ad872f
i05nagai <i05nagai@gmail.com>
2018-06-05 22:10:44 +0900

    Add features to make package

--- mafipy/__init__.py
+++ mafipy/__init__.py
@@ -13,9 +13,27 @@ from . import function
 from . import math
 from . import replication
 
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#   X.Y     # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+__version__ = '0.1.dev2'
+
 __all__ = [
-    "calibrator",
-    "function",
-    "replication",
+    'calibrator',
+    'function',
     'math',
+    'replication',
 ]
---
+++ scripts/packaging_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+
+set -e
+
+#
+#
+#
+eval "$(pyenv init -)"
+pyenv virtualenv-delete mafipy_install
+pyenv virtualenv 3.5.2 mafipy_install
+pyenv activate mafipy_install
+pip install --upgrade pip
+pip install \
+  --index-url https://test.pypi.org/simple/ \
+  --extra-index-url https://testpypi.python.org/pypi \
+  --extra-index-url https://pypi.org/simple/ \
+  mafipy==0.1.dev2
+pip install \
+  matplotlib==2.2.2
+
+#
+# run examples
+#
+python examples/plot_black_scholes_european_vanilla_option.py
+python examples/plot_quanto_cms_bull_spread.py
+python examples/plot_smile_curve_sabr.py
